f675c73ea07131327a3322f5bc8f003892716790
Saymon Luna <saymonpersonal2004@gmail.com>
2026-07-15 18:31:31 -0300

    Create Armors.lua

---
+++ Data/ItemsInformations/Armors.lua
@@ -0,0 +1,202 @@
+return {
+	-- HELMETS --
+
+	["AR001"] = {
+		Id = "AR001",
+		Name = "Leather Hood",
+		Category = "Armor",
+		ArmorType = "Helmet",
+		Rarity = "Common",
+		Level = 1,
+		Value = 5,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 2
+	},
+
+	["AR002"] = {
+		Id = "AR002",
+		Name = "Iron Helmet",
+		Category = "Armor",
+		ArmorType = "Helmet",
+		Rarity = "Uncommon",
+		Level = 6,
+		Value = 20,
+		ImageId = "rbxassetid://90031540078253",
+		MaxAmount = 1,
+		Armor = 6
+	},
+
+	["AR003"] = {
+		Id = "AR003",
+		Name = "Knight Helmet",
+		Category = "Armor",
+		ArmorType = "Helmet",
+		Rarity = "Rare",
+		Level = 15,
+		Value = 70,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 12
+	},
+
+	["AR004"] = {
+		Id = "AR004",
+		Name = "Golden Crown",
+		Category = "Armor",
+		ArmorType = "Helmet",
+		Rarity = "Epic",
+		Level = 30,
+		Value = 250,
+		ImageId = "rbxassetid://132592398488663",
+		MaxAmount = 1,
+		Armor = 20
+	},
+
+	["AR005"] = {
+		Id = "AR005",
+		Name = "Dragon Helm",
+		Category = "Armor",
+		ArmorType = "Helmet",
+		Rarity = "Legendary",
+		Level = 45,
+		Value = 800,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 35
+	},
+
+	-- UPPER --
+
+	["AR006"] = {
+		Id = "AR006",
+		Name = "Leather Armor",
+		Category = "Armor",
+		ArmorType = "Upper",
+		Rarity = "Common",
+		Level = 1,
+		Value = 10,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 5
+	},
+
+	["AR007"] = {
+		Id = "AR007",
+		Name = "Iron Chestplate",
+		Category = "Armor",
+		ArmorType = "Upper",
+		Rarity = "Uncommon",
+		Level = 8,
+		Value = 35,
+		ImageId = "rbxassetid://109877531337028",
+		MaxAmount = 1,
+		Armor = 12
+	},
+
+	["AR008"] = {
+		Id = "AR008",
+		Name = "Knight Chestplate",
+		Category = "Armor",
+		ArmorType = "Upper",
+		Rarity = "Rare",
+		Level = 18,
+		Value = 120,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 22
+	},
+
+	["AR009"] = {
+		Id = "AR009",
+		Name = "Royal Armor",
+		Category = "Armor",
+		ArmorType = "Upper",
+		Rarity = "Epic",
+		Level = 32,
+		Value = 350,
+		ImageId = "rbxassetid://103017692037713",
+		MaxAmount = 1,
+		Armor = 36
+	},
+
+	["AR010"] = {
+		Id = "AR010",
+		Name = "Dragon Chestplate",
+		Category = "Armor",
+		ArmorType = "Upper",
+		Rarity = "Legendary",
+		Level = 50,
+		Value = 1200,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 55
+	},
+
+	-- LOWER --
+
+	["AR011"] = {
+		Id = "AR011",
+		Name = "Leather Pants",
+		Category = "Armor",
+		ArmorType = "Lower",
+		Rarity = "Common",
+		Level = 1,
+		Value = 8,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 4
+	},
+
+	["AR012"] = {
+		Id = "AR012",
+		Name = "Iron Leggings",
+		Category = "Armor",
+		ArmorType = "Lower",
+		Rarity = "Uncommon",
+		Level = 8,
+		Value = 30,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 10
+	},
+
+	["AR013"] = {
+		Id = "AR013",
+		Name = "Knight Leggings",
+		Category = "Armor",
+		ArmorType = "Lower",
+		Rarity = "Rare",
+		Level = 18,
+		Value = 110,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 18
+	},
+
+	["AR014"] = {
+		Id = "AR014",
+		Name = "Royal Leggings",
+		Category = "Armor",
+		ArmorType = "Lower",
+		Rarity = "Epic",
+		Level = 32,
+		Value = 320,
+		ImageId = "rbxassetid://103243904443126",
+		MaxAmount = 1,
+		Armor = 30
+	},
+
+	["AR015"] = {
+		Id = "AR015",
+		Name = "Dragon Leggings",
+		Category = "Armor",
+		ArmorType = "Lower",
+		Rarity = "Legendary",
+		Level = 50,
+		Value = 1100,
+		ImageId = "",
+		MaxAmount = 1,
+		Armor = 48
+	},
+}
