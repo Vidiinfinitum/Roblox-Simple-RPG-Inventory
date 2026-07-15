c763a44940c256a1b0dc2322336cfe6ccb6bf04c
Saymon Luna <saymonpersonal2004@gmail.com>
2026-07-15 18:29:40 -0300

    Create PlayerEquipment.lua

---
+++ Data/PlayerEquipment.lua
@@ -0,0 +1,32 @@
+return {
+	Weapon = {
+		InstanceId = "7KF29A1",
+		Id = "W004",
+	},
+
+	Armor = {
+		Helmet = {
+			IstanceId = "X8P41LM",
+			Id = "AR004",
+		},
+		Upper = {
+			InstanceId = "N3Q8ZTR",
+			Id = "AR009",
+		},
+		Lower = {
+			InstanceId = "V4M72BK",
+			Id = "AR014",
+		},
+	},
+
+	Accessories = {
+		Accessory1 = {
+			InstanceId = "A91XF3P",
+			Id = "AC005",
+		},
+		Accessory2 = {
+			InstanceId = "L5R8YQ2",
+			Id = "AC009",
+		},
+	}
+}
