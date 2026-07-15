f12b56f5c8e7da1e0f2b71fa82b7289c2c522d2e
Saymon Luna <saymonpersonal2004@gmail.com>
2026-07-15 18:30:32 -0300

    Create PlayerInventory.lua

---
+++ Data/PlayerInventory.lua
@@ -0,0 +1,114 @@
+return {
+
+	["7KF29A1"] = {
+		InstanceId = "7KF29A1",
+		Category = "Weapon",
+		Equiped = true,
+		Id = "W004",
+		Amount = 1,
+	},
+
+	["T2M9KD8"] = {
+		InstanceId = "T2M9KD8",
+		Category = "Weapon",
+		Equiped = false,
+		Id = "W002",
+		Amount = 1,
+	},
+
+	["P7X3NC5"] = {
+		InstanceId = "P7X3NC5",
+		Category = "Weapon",
+		Equiped = false,
+		Id = "W010",
+		Amount = 1,
+	},
+
+	-- Armors
+	["X8P41LM"] = {
+		InstanceId = "X8P41LM",
+		Category = "Armor",
+		Equiped = true,
+		Id = "AR004",
+		Amount = 1,
+	},
+
+	["N3Q8ZTR"] = {
+		InstanceId = "N3Q8ZTR",
+		Category = "Armor",
+		Equiped = true,
+		Id = "AR009",
+		Amount = 1,
+	},
+
+	["V4M72BK"] = {
+		InstanceId = "V4M72BK",
+		Category = "Armor",
+		Equiped = true,
+		Id = "AR014",
+		Amount = 1,
+	},
+
+	["H8Q2BZ6"] = {
+		InstanceId = "H8Q2BZ6",
+		Category = "Armor",
+		Equiped = false,
+		Id = "AR002",
+		Amount = 1,
+	},
+
+	["J4V9LW1"] = {
+		InstanceId = "J4V9LW1",
+		Category = "Armor",
+		Equiped = false,
+		Id = "AR007",
+		Amount = 1,
+	},
+
+	-- Accessories
+	["A91XF3P"] = {
+		InstanceId = "A91XF3P",
+		Category = "Accessory",
+		Equiped = true,
+		Id = "AC005",
+		Amount = 1,
+	},
+
+	["L5R8YQ2"] = {
+		InstanceId = "L5R8YQ2",
+		Category = "Accessory",
+		Equiped = true,
+		Id = "AC009",
+		Amount = 1,
+	},
+
+	["C6N1TP4"] = {
+		InstanceId = "C6N1TP4",
+		Category = "Accessory",
+		Equiped = false,
+		Id = "AC003",
+		Amount = 1,
+	},
+
+	-- Consumables
+	["M9K4RX7"] = {
+		InstanceId = "M9K4RX7",
+		Category = "Consumable",
+		Id = "C003",
+		Amount = 10,
+	},
+
+	["W2L8FH9"] = {
+		InstanceId = "W2L8FH9",
+		Category = "Consumable",
+		Id = "C008",
+		Amount = 3,
+	},
+
+	["Q5D7SV1"] = {
+		InstanceId = "Q5D7SV1",
+		Category = "Consumable",
+		Id = "C012",
+		Amount = 1,
+	},
+}
