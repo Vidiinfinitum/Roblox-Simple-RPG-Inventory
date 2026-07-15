f1d7519b63d6e64020c4122503c1b12bf4b860fa
Saymon Luna <saymonpersonal2004@gmail.com>
2026-07-15 18:31:15 -0300

    Create Weapons.lua

---
+++ Data/ItemsInformations/Weapons.lua
@@ -0,0 +1,215 @@
+return {
+	-- SWORDS --
+
+	["W001"] = {
+		Id = "W001",
+		Name = "Wood Sword",
+		Category = "Weapon",
+		WeaponType = "Sword",
+		Rarity = "Common",
+		Level = 1,
+		Value = 3,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 2
+	},
+
+	["W002"] = {
+		Id = "W002",
+		Name = "Stone Sword",
+		Category = "Weapon",
+		WeaponType = "Sword",
+		Rarity = "Common",
+		Level = 3,
+		Value = 8,
+		ImageId = "rbxassetid://124715984007220",
+		MaxAmount = 1,
+		Damage = 5
+	},
+
+	["W003"] = {
+		Id = "W003",
+		Name = "Iron Sword",
+		Category = "Weapon",
+		WeaponType = "Sword",
+		Rarity = "Uncommon",
+		Level = 8,
+		Value = 25,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 11
+	},
+
+	["W004"] = {
+		Id = "W004",
+		Name = "Steel Sword",
+		Category = "Weapon",
+		WeaponType = "Sword",
+		Rarity = "Rare",
+		Level = 15,
+		Value = 60,
+		ImageId = "rbxassetid://112490367676395",
+		MaxAmount = 1,
+		Damage = 19
+	},
+
+	["W005"] = {
+		Id = "W005",
+		Name = "Knight Sword",
+		Category = "Weapon",
+		WeaponType = "Sword",
+		Rarity = "Epic",
+		Level = 25,
+		Value = 180,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 34
+	},
+
+	["W006"] = {
+		Id = "W006",
+		Name = "Dragon Slayer",
+		Category = "Weapon",
+		WeaponType = "Sword",
+		Rarity = "Legendary",
+		Level = 40,
+		Value = 750,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 60
+	},
+
+	-- AXES --
+
+	["W007"] = {
+		Id = "W007",
+		Name = "Wood Axe",
+		Category = "Weapon",
+		WeaponType = "Axe",
+		Rarity = "Common",
+		Level = 2,
+		Value = 4,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 4
+	},
+
+	["W008"] = {
+		Id = "W008",
+		Name = "Stone Axe",
+		Category = "Weapon",
+		WeaponType = "Axe",
+		Rarity = "Common",
+		Level = 5,
+		Value = 12,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 8
+	},
+
+	["W009"] = {
+		Id = "W009",
+		Name = "Battle Axe",
+		Category = "Weapon",
+		WeaponType = "Axe",
+		Rarity = "Rare",
+		Level = 16,
+		Value = 90,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 23
+	},
+
+	["W010"] = {
+		Id = "W010",
+		Name = "Executioner's Axe",
+		Category = "Weapon",
+		WeaponType = "Axe",
+		Rarity = "Epic",
+		Level = 28,
+		Value = 230,
+		ImageId = "rbxassetid://80821115973609",
+		MaxAmount = 1,
+		Damage = 39
+	},
+
+	["W011"] = {
+		Id = "W011",
+		Name = "Titan Axe",
+		Category = "Weapon",
+		WeaponType = "Axe",
+		Rarity = "Legendary",
+		Level = 45,
+		Value = 900,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 68
+	},
+
+	-- SPEARS --
+
+	["W012"] = {
+		Id = "W012",
+		Name = "Training Spear",
+		Category = "Weapon",
+		WeaponType = "Spear",
+		Rarity = "Common",
+		Level = 1,
+		Value = 5,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 3
+	},
+
+	["W013"] = {
+		Id = "W013",
+		Name = "Iron Spear",
+		Category = "Weapon",
+		WeaponType = "Spear",
+		Rarity = "Uncommon",
+		Level = 7,
+		Value = 22,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 10
+	},
+
+	["W014"] = {
+		Id = "W014",
+		Name = "Knight Spear",
+		Category = "Weapon",
+		WeaponType = "Spear",
+		Rarity = "Rare",
+		Level = 18,
+		Value = 95,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 24
+	},
+
+	["W015"] = {
+		Id = "W015",
+		Name = "Royal Halberd",
+		Category = "Weapon",
+		WeaponType = "Spear",
+		Rarity = "Epic",
+		Level = 30,
+		Value = 300,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 42
+	},
+
+	["W016"] = {
+		Id = "W016",
+		Name = "Spear of Eternity",
+		Category = "Weapon",
+		WeaponType = "Spear",
+		Rarity = "Legendary",
+		Level = 50,
+		Value = 1200,
+		ImageId = "",
+		MaxAmount = 1,
+		Damage = 75
+	},
+}
